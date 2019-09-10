685bba99be87cfe1f936ebe5e230a3e678ffa648
Whitney <wmeulink@gmail.com>
2019-09-10 14:50:42 -0700

    adding new project

---
+++ RemoveDups/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace RemoveDups
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void RemoveDuplicates(LinkedList<string> node) {
+
+        }
+    }
+}
