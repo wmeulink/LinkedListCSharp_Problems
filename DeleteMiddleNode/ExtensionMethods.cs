9306898729824ea7cfc92690a05bd2608f689ef6
Whitney <wmeulink@gmail.com>
2019-09-12 14:17:13 -0700

    finished delete middle node

---
+++ DeleteMiddleNode/ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeleteMiddleNode
+{
+    public static class ExtensionMethods
+    {
+        public static void DeleteMiddleNode<T>(this LinkedList<T> source)
+        {
+            if (source == null || source.Count == 0) return;
+
+            if (source.Count % 2 == 0) return;
+
+            var middle = source.Count / 2;
+            var currentNode = source.First;
+            for (int i = 0; i < middle; i++)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            source.Remove(currentNode);
+
+        }
+
+    }
+}
---
+++ DeleteMiddleNode/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DeleteMiddleNode
+{
+
+    /*  
+     *  Implement an algorithm to delete a node in the middle (Le., any node but
+        the first and last node, not necessarily the exact middle) of a singly linked list, given only access to
+        that node.
+        EXAMPLE
+        Input: the node c from the linked list a - >b- >c - >d - >e- >f
+        Result: nothing is returned, but the new linked list looks like a->b->d->e->f
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new LinkedList<string>(new[] { "a", "b", "c", "b", "a" });
+            var oldSource = source;
+            
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sbOld = new StringBuilder();
+            foreach (string s in oldSource)
+            {
+                sbOld.Append(s);
+            }
+            source.DeleteMiddleNode<String>();
+            foreach (string s in source)
+            {
+                sb.Append(s);
+            }
+            
+
+            Console.WriteLine("Here is your old list: {0}", sbOld.ToString());
+            Console.WriteLine("Here is your new list: {0}", sb.ToString());
+        }
+    }
+}
