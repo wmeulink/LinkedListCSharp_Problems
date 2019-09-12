654d0b0212a9c140167a52ce3bebae7e9077f93f
Whitney <wmeulink@gmail.com>
2019-09-12 13:57:18 -0700

    finished with KFromLast

---
+++ KFromLast/ExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KFromLast
+{
+   public static class ExtensionMethods
+    {
+        public static IEnumerable<T> KFromLast<T>(this LinkedList<T> source, int k)
+        {
+            if (source == null || source.Count == 0) yield break;
+
+            int length = source.Count;
+            var currentNode = source.First;
+            for (int i = 0; i < length - k; i++)
+            {
+                currentNode = currentNode.Next;
+            }
+            while (currentNode != null)
+            {
+                yield return currentNode.Value;
+                currentNode = currentNode.Next;
+            }
+        }
+
+
+
+    }
+}
---
+++ KFromLast/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KFromLast
+{
+    class Program
+    {
+
+        /*Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list*/
+
+        static void Main(string[] args)
+        {
+            var source = new LinkedList<String>(new[] { "a", "b", "c", "d", "e" });
+            var k = 3;
+
+            var expected = new[] { "c", "d", "e" };
+
+            var result = source.KFromLast(k);
+
+            StringBuilder source_sb = new StringBuilder();
+            StringBuilder expected_sb = new StringBuilder();
+            foreach (string x in source)
+            {
+                source_sb.Append(x);
+            }
+            foreach (string x in expected)
+            {
+                expected_sb.Append(x);
+            }
+
+            Console.WriteLine("Here is the old LinkedList {0}", source_sb.ToString());
+            Console.WriteLine("Here are the last {0} items in the LinkedList {1}", k, expected_sb.ToString());
+        }
+
+    }
+
+
+    
+}
