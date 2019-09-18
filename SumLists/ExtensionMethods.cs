6287d94951b9c1e01bedfe6bcbdc0e57b73d3776
Whitney <wmeulink@gmail.com>
2019-09-18 12:51:28 -0700

    finished SumLists. Partition problem didn't make sense to me, so I am not doing it I suppose

---
+++ SumLists/ExtensionMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SumLists
+{
+    public static class ExtensionMethods
+    {
+        public static LinkedList<int> SumLists(this LinkedList<int> num1, LinkedList<int> num2)
+        {
+            var node1 = num1.Last;
+            var node2 = num2.Last;
+
+            var result = new LinkedList<int>();
+
+            var carry = 0;
+
+            while (true)
+            {
+                if (node1 == null && node2 == null && carry == 0)
+                {
+                    break;
+                }
+                var val1 = node1?.Value ?? 0;
+                var val2 = node2?.Value ?? 0;
+
+                var sum = val1 + val2 + carry;
+                carry = sum / 10;
+                result.AddFirst(sum % 10);
+
+                node1 = node1?.Previous;
+                node2 = node2?.Previous;
+
+
+            }
+            return result;
+        }
+    }
+}
---
+++ SumLists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumLists
+{
+    class Program
+    {
+
+        /*
+         Sum Lists: You have two numbers represented by a linked list, where each node contains a single
+        digit. The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a
+        function that adds the two numbers and returns the sum as a linked list.
+            EXAMPLE
+                Input: (7-> 1 -> 6) + (5 -> 9 -> 2) .Thatis,617 + 295.
+                Output: 2 - > 1 - > 9.That is, 912.
+            FOLLOW UP   
+                Suppose the digits are stored in forward order. Repeat the above problem.
+                Input: (6 -> 1 -> 7) + (2 -> 9 -> 5).Thatis,617 + 295.
+                Output: 9 - > 1 - > 2.That is, 912
+             
+             */
+        static void Main(string[] args)
+        {
+            var number1 = new LinkedList<int>(new[] { 7, 9, 2, 3 });
+            var number2 = new LinkedList<int>(new[] { 2, 1, 9, 8 });
+
+            var expected = new LinkedList<int>(new[] { 1, 0, 1, 2, 1 });
+
+            var result = number1.SumLists(number2);
+            foreach (int i in result)
+            {
+                Console.WriteLine("The first number is {0}", i);
+                
+            }
+            foreach (int i in expected)
+            {
+                Console.WriteLine("The first number in the expected result is {0}", i);
+            }
+
+        }
+
+    }
+}
